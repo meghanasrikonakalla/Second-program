d44e0bd47ce10f2290407ef5f8f2a98be90a42e6
Meghana Sri Konakalla <konakallameghana@gmail.com>
2026-03-20 15:35:04 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,41 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=4
+>>> type(a)
+<class 'int'>
+>>> b=30
+>>> type(b)
+<class 'int'>
+>>> c=6.7
+>>> type(c)
+<class 'float'>
+>>> d='Hello'
+>>> type(d)
+<class 'str'>
+>>> e="Meghana"
+>>> type(e)
+<class 'str'>
+>>> f='''hi'''
+>>> type(f)
+<class 'str'>
+>>> g=2+5j
+>>> type(g)
+<class 'complex'>
+>>> h=5j+7
+>>> type(h)
+<class 'complex'>
+>>> i=7
+>>> type(i)
+<class 'int'>
+>>> i=6j
+>>> type(j)
+Traceback (most recent call last):
+  File "<pyshell#19>", line 1, in <module>
+    type(j)
+NameError: name 'j' is not defined
+>>> e=True
+>>> type(e)
+<class 'bool'>
+>>> f=False
+>>> type(f)
+<class 'bool'>
