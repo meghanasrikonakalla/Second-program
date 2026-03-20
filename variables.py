9496514acc41f4d3d2dd3d4fbd0323b74ad1e6aa
Meghana Sri Konakalla <konakallameghana@gmail.com>
2026-03-20 15:34:37 +0530

    Add files via upload
    In Second day of coding I have gained Knowledge regarding Variables,Data Types,Data Type Conversions

---
+++ variables.py
@@ -0,0 +1,99 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#variables
+a=10
+print(a)
+10
+print(4+6)
+10
+a=3
+b=9
+print(a+b)
+12
+c=30
+print(C)
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    print(C)
+NameError: name 'C' is not defined. Did you mean: 'c'?
+print(c)
+30
+2=8
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+a0123456789=100
+>>> print(a0123456789)
+100
+>>> name="Meghana"
+>>> print(name)
+Meghana
+>>> city="VJA"
+>>> print(city)
+VJA
+>>> course="pyhton"
+>>> print(course)
+pyhton
+>>> #keywords
+>>> if=100
+SyntaxError: invalid syntax
+>>> elif=30
+SyntaxError: invalid syntax
+>>> a=2,b=5
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+>>> a=2;b=5
+>>> print(a,b)
+2 5
+>>> a,b=6,7
+>>> print(a+b)
+13
+>>> c=30
+>>> ptint(c)
+Traceback (most recent call last):
+  File "<pyshell#29>", line 1, in <module>
+    ptint(c)
+NameError: name 'ptint' is not defined. Did you mean: 'print'?
+>>> print(c)
+30
+>>> a,b="python","course"
+>>> print(a+b)
+pythoncourse
+>>> print(a+" "+b)
+python course
+>>> fname="Meghana"
+>>> lname="Konakalla"
+>>> print(fname+" "+lname)
+Meghana Konakalla
+>>> #special Character
+>>> @a=6
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+>>> _a=100
+>>> print(_a)
+100
+_=30
+print(_)
+30
+ a=9
+ 
+SyntaxError: unexpected indent
+first name="Meghana"
+SyntaxError: invalid syntax
+firstname="Meghana"
+print(firstname)
+Meghana
+first_name="Meghana"
+print(first_name)
+Meghana
+a=6
+print(a)
+6
+del a
+a
+Traceback (most recent call last):
+  File "<pyshell#52>", line 1, in <module>
+    a
+NameError: name 'a' is not defined. Did you mean: '_a'?
+NAME="Meghana"
+print(NAME)
+Meghana
+Name="Meghana"
+print(Name)
+Meghana
